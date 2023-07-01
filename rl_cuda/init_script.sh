88b1114931d88d84c94e7c44b2c9f0d24624119f
spacegoing <spacegoing@gmail.com>
2023-07-01 20:37:17 +0000

    [New] init commit

---
+++ rl_cuda/init_script.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Generate SSH keys
+ssh-keygen -t rsa -b 4096 -N "" -f ~/.ssh/id_rsa
+cat ~/.ssh/id_rsa.pub
+
+# Set a var to dir
+ConfigDir=/root/myCodeLab/GitConfig
+
+# Create several symlinks
+ln -s ${ConfigDir}/tmux/.tmux.conf ${HOME}/.tmux.conf
+ln -s ${ConfigDir}/.bin/tmux/tat ${HOME}/bin/tat
+ln -s ${ConfigDir}/.bin/tmux/tkl ${HOME}/bin/tkl
+ln -s ${ConfigDir}/.bin/tmux/tnew ${HOME}/bin/tnew
+ln -s ${ConfigDir}/.bin/dev/pyservesg ${HOME}/bin/pyservesg
+ln -s ${ConfigDir}/.bin/dev/scpray ${HOME}/bin/scpray
+ln -s ${ConfigDir}/.bin/dev/sgpdb ${HOME}/bin/sgpdb
+ln -s ${ConfigDir}/.bin/docker/dbash ${HOME}/bin/dbash
+ln -s ${ConfigDir}/.bin/docker/drmrf ${HOME}/bin/drmrf
+ln -s ${ConfigDir}/.bin/tmux/tsgs/tsg ${HOME}/bin/tsg
+
+# Create Docker Compose related alias / shortcuts
+echo 'alias dcu="docker compose up -d"' >> ${HOME}/.bashrc
+echo 'alias dcd="docker compose down"' >> ${HOME}/.bashrc
+echo 'alias dcps="docker compose ps"' >> ${HOME}/.bashrc
+echo 'alias dclogs="docker compose logs"' >> ${HOME}/.bashrc
+
+# Source the .bashrc to apply changes
+source ${HOME}/.bashrc
