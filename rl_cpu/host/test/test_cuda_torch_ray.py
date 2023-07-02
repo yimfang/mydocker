3b0684de920eff1c44a3b8b6da0797c82cb6e402
spacegoing <spacegoing@gmail.com>
2023-07-02 14:34:17 +0000

    [New] compose file for cpu

---
+++ rl_cpu/host/test/test_cuda_torch_ray.py
@@ -0,0 +1,63 @@
+import torch
+import ray
+
+
+def test_pytorch():
+  print('Testing PyTorch...')
+  if torch.cuda.is_available():
+    print("PyTorch can use GPUs!")
+
+    # Check the number of GPUs available
+    num_gpus = torch.cuda.device_count()
+    print(f"Number of GPUs available: {num_gpus}")
+
+    try:
+      # Loop over all available GPUs and print their specifications
+      for i in range(num_gpus):
+        device = torch.device(f"cuda:{i}")
+        gpu_properties = torch.cuda.get_device_properties(device)
+        print(f"\nGPU {i} specifications:")
+        print(f"  Name: {gpu_properties.name}")
+        print(
+            f"  Compute Capability: {gpu_properties.major}.{gpu_properties.minor}"
+        )
+        print(f"  Total memory (GB): {gpu_properties.total_memory / 1e9}")
+        print(f"  Multiprocessors: {gpu_properties.multi_processor_count}")
+
+      # Test PyTorch's ability to perform calculations on the GPU
+      test_tensor = torch.tensor([1.0, 2.0]).cuda()
+      print("\nPerforming operations on GPU...")
+      print(f"Original tensor: {test_tensor}")
+      print(f"Tensor after addition: {test_tensor + 2}")
+      print("All PyTorch functionalities are working on the GPU!")
+
+      print('PyTorch can access the GPU.')
+    except RuntimeError as e:
+      print('Error performing a calculation on the GPU:', e)
+  else:
+    test_tensor = torch.tensor([1.0, 2.0])
+    print("\nPerforming operations on CPU...")
+    print(f"Original tensor: {test_tensor}")
+    print(f"Tensor after addition: {test_tensor + 2}")
+    print('Success! CPU Working!')
+
+
+def test_ray():
+  print('#'*30)
+  print('\n\nTesting Ray...')
+  ray.init()
+
+  @ray.remote
+  def f():
+    return torch.rand(1000, 1000).sum()
+
+  try:
+    result = ray.get(f.remote())
+    print('Ray can perform calculations on the CPU.')
+  except RuntimeError as e:
+    print('Error performing a calculation on the CPU:', e)
+
+
+if __name__ == "__main__":
+  test_pytorch()
+  test_ray()
